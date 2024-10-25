a1f1e6eab3171687ab65076dcbe07833835fad04
Guilherme Barbiero <133436901+GuiBarbiero@users.noreply.github.com>
2024-10-24 21:08:09 -0300

    Update FilmesAssistidos.jsx

--- src/pages/FilmesAssistidos.jsx
+++ src/pages/FilmesAssistidos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getWatchedMovies, removeFromWatched } from '../utils/localStorage';
+import { getWatchedMovies, removeFromWatched } from '../Utils/localStorage.js';
 import MovieCard from '../components/MovieCard';
 
 export default function WatchedMoviesPage() {
